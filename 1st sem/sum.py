7e635a6607e2c7138901a551033577e93feac20e
tuhin <mylifecodding@gmail.com>
2023-03-04 10:30:44 +0530

    sum with and without using function

--- 1st sem/sum.py
+++ 1st sem/sum.py
@@ -1,4 +1,12 @@
 # sum using function
-list = [1,2,3,4,5,6]
-add = sum(list)
-print(add)
+list1 = [1,2,3,4,5,6]
+add1 = sum(list1)
+print("sum with using a function ",add1)
+
+# with out function
+list2 = [1,2,3,4,5,6]
+temp = 0
+length = 6 #this is the number of element of list 2
+for i in range(length):
+    temp = temp + list2[i]
+print(f"sum without using a functon {temp}") #this is the formatted printing type
